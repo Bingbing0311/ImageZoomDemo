3c6173df3a730df7138f46c438aa4e676663320d
Vicky <Bingbing@bingdemac-mini.local>
2016-01-12 15:46:17 +0800

    Initial Commit

---
+++ ImageZoomDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageZoomDemo
+//
+//  Created by Bing on 16/1/12.
+//  Copyright © 2016年 Bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageZoomDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageZoomDemo
+//
+//  Created by Bing on 16/1/12.
+//  Copyright © 2016年 Bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
